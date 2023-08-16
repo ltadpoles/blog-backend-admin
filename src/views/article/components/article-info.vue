7377ef34b56a293442bb3b1be9b2ffdfbaef66cd
tadpole <ltadpole@163.com>
2023-08-17 00:17:20 +0800

    article

--- src/views/article/components/article-info.vue
+++ src/views/article/components/article-info.vue
@@ -1,7 +1,16 @@
 <template>
     <v-dialog :isShow="isShow" :title="title" @close="close" fullscreen>
         <div class="article-info">
-            <div class="article-info-title">我的第一篇文章</div>
+            <div class="flex-center">
+                <div class="article-info-header">
+                    <el-space wrap>
+                        <el-tag>技术研究</el-tag>
+                        <el-tag type="success">vue</el-tag>
+                        <el-tag type="success">javaScript</el-tag>
+                        <span>发布于 2023-2-18</span>
+                    </el-space>
+                </div>
+            </div>
         </div>
     </v-dialog>
 </template>
@@ -26,5 +35,19 @@ const close = () => {
 </script>
 
 <style lang="less" scoped>
-.article-info {}
+.article-info {
+    display: flex;
+    justify-content: center;
+    padding: 30px 0;
+
+    .flex-center {
+        min-width: 860px;
+        max-width: 860px;
+        width: 860px;
+    }
+
+    .article-info-header {
+        color: var(--el-text-color-secondary);
+    }
+}
 </style>>
